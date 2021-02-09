d529e4775b559fddc52a2c27308d86dc39fbd291
Justin Z <contactjustinzamora@gmail.com>
2021-02-08 18:51:51 -0800

    installed react-router-dom

---
+++ src/components/VendingMachine.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {BrowserRoute, Route, Link} from 'react-router-dom'
+
+const VendingMachine = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default VendingMachine
